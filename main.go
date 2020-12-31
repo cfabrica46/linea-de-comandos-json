6e4d7a68c4638b9f3d869c481e7fad736ed06ce8
César <c.caycho46@gmail.com>
2020-12-30 19:11:59 -0500

    login terminado

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -13,6 +15,10 @@ type user struct {
 }
 
 func main() {
+
+	var data []byte
+	var users []user
+	var acceso bool
 
 	login := flag.NewFlagSet("login", flag.ExitOnError)
 
@@ -34,14 +40,6 @@ login Para ingresar`
 
 	}
 
-	switch os.Args[1] {
-	case "login":
-		username := login.String("username", "", "Introduce el username que desees")
-		password := login.String("password", "", "Introduce la password que desees")
-		login.Parse(os.Args[2:])
-		fmt.Println(*username, *password)
-	}
-
 	archivo, err := os.OpenFile("databases.json", os.O_RDWR, 0644)
 
 	if err != nil {
@@ -49,5 +47,49 @@ login Para ingresar`
 	}
 
 	defer archivo.Close()
+
+	buf := make([]byte, 512)
+
+	for {
+		n, err := archivo.Read(buf)
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+		data = append(data, buf[:n]...)
+	}
+
+	err = json.Unmarshal(data, &users)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch os.Args[1] {
+	case "login":
+		username := login.String("username", "", "Introduce el username que desees")
+		password := login.String("password", "", "Introduce la password que desees")
+		login.Parse(os.Args[2:])
+
+		for i := range users {
+
+			if users[i].Name == *username && users[i].Password == *password {
+
+				acceso = true
+
+			}
+
+		}
+
+		if acceso == true {
+			fmt.Println("Bienvenido", *username)
+		} else {
+			fmt.Println("Username y/o Password incorrectas")
+		}
+
+	}
 
 }
